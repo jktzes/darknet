e80aa379245b78e996342209d741f57dba557b13
Ezo Tang <jktzes@gmail.com>
2018-07-22 15:15:36 -0500

    added conditional commands based on env

--- make_crow.sh
+++ make_crow.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-clang++ -std=c++11 -lboost_system -pthread -o crow crow.cpp darknet.so 
+if [ "$(uname)" == "Darwin" ]; then
+	clang++ -std=c++11 -lboost_system -pthread -o crow crow.cpp darknet.so 
+elif [ "$(uname)" == "Linux" ]; then
+	g++ -std=c++11 -lboost_system -pthread -o crow crow.cpp darknet.so 
+fi
