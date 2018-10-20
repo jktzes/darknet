111d3fae3bf4615c28d1b7c7b36bc13540e770fc
Ubuntu <ubuntu@ip-172-31-46-135.ec2.internal>
2018-10-20 09:13:05 +0000

    added a draft of pistache rest with rapid support

--- compile_pistache.sh
+++ compile_pistache.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ pistache_sample.cpp -lpistache -lpthread -std=c++11 -o pistache_sample
+g++ rapid_pistache.cpp -lpistache -lpthread -std=c++11 -o rapid_pistache
---
+++ rapid_pistache.cpp
@@ -0,0 +1,53 @@
+#include <pistache/endpoint.h>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+
+using namespace Pistache;
+
+class HelloHandler : public Http::Handler {
+public:
+
+    HTTP_PROTOTYPE(HelloHandler)
+
+    void onRequest(const Http::Request& request, Http::ResponseWriter response) {
+        UNUSED(request);
+        response.send(Pistache::Http::Code::Ok, generate_json());
+    }
+};
+
+std::string generate_json() {
+    using namespace rapidjson;
+	Document document;
+	Value something;
+	Value somearray;
+	Document::AllocatorType& allocator = document.GetAllocator();
+
+	document.SetObject();
+	something.SetObject();
+	somearray.SetArray();
+
+	somearray.PushBack("woah", allocator); 
+	document.AddMember("array", somearray, allocator);
+	document.AddMember("something", something, allocator);
+	document.AddMember("it", "works", allocator);
+
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	document.Accept(writer);
+	return buffer.GetString();
+}
+
+int main() {
+    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(3205));
+    auto opts = Pistache::Http::Endpoint::options()
+        .threads(1);
+
+    Http::Endpoint server(addr);
+    server.init(opts);
+    server.setHandler(Http::make_handler<HelloHandler>());
+    server.serve();
+
+    server.shutdown();
+}
